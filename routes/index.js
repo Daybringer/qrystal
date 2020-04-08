87693508558ec01381d9b36a6367560f9e74457f
Daybringer <vanata.michal@gmail.com>
2020-04-08 16:13:51 +0200

    truncated version of pisiskvorky

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { ensureAuthenticated } = require("../config/auth");
 
 // Welcome page
 router.get("/", (req, res) => {
-  res.render("welcome");
-});
-
-// Dashboard
-router.get("/dashboard", ensureAuthenticated, (req, res) => {
-  res.render("dashboard", {
-    username: req.user.username
-  });
+  res.render("dashboard");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,10 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv/config');
-}
+
 
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
-const mongoose = require('mongoose');
-const flash = require("connect-flash");
-const session = require("express-session");
-const passport = require("passport");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-
-// Passport config
-require('./config/passport')(passport);
 
 // EJS
 app.use(expressLayouts);
@@ -23,47 +13,8 @@ app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 
-// Bodyparser
-app.use(express.urlencoded({ extended: false }));
-
-// Express Session 
-app.use(
-  session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-  })
-);
-
-// Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Connect flash
-app.use(flash());
-
-// Global vars
-app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash("success_msg");
-  res.locals.error_msg = req.flash("error_msg");
-  res.locals.error = req.flash("error");
-  next();
-})
-
 // Routing
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
-//MongoDB Constructor and URL parser deprecation warning fix
-mongoose.set('useUnifiedTopology', true);
-mongoose.set('useNewUrlParser', true);
-
-// DB connection
-mongoose.connect(process.env.DB_CONNECTION);
-const db = mongoose.connection;
-
-db.on("error", error => console.error(error));
-db.once("open", () => console.log("Connected to Mongoose"));
 
 // 404 Error page
 app.use(function (req, res, next) {
